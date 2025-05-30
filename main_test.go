c8114ed90fc27874692f4bfc9d035f278b47de00
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-05-30 21:30:01 +0900

    Add test for `(*Markdown) EnableReadmeToIndex`

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEnableReadmeToIndex1(t *testing.T) {
+	M := New("#")
+	M.EnableReadmeToIndex()
+
+	result := M.filter([]byte("[README_ja](./README_ja.md)"))
+	expect := []byte("[README_ja](./index_ja.html)")
+
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("expect %v, but %v", string(expect), string(result))
+	}
+}
+
+func TestEnableReadmeToIndex2(t *testing.T) {
+	M := New("#")
+	M.EnableReadmeToIndex()
+
+	result := M.filter([]byte("[README_ja]: ./README_ja.md"))
+	expect := []byte("[README_ja]: ./index_ja.html")
+
+	if !bytes.Equal(result, expect) {
+		t.Fatalf("expect %v, but %v", string(expect), string(result))
+	}
+}

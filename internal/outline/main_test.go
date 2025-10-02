dfbedfa9bae95935488f1ca05ace78e0dad22834
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-10-03 00:15:30 +0900

    Fix the test of internal/outline

--- internal/outline/main_test.go
+++ internal/outline/main_test.go
@@ -20,7 +20,7 @@ SQL-Bless
 		&Header{Level: 2, Title: "サポートコマンド", ID: "%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89"},
 		&Header{Level: 3, Title: "scoop インストーラーを使用する場合", ID: "scoop-%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%A9%E3%83%BC%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B%E5%A0%B4%E5%90%88"},
 	}
-	result, err := makeOutline(strings.NewReader(source))
+	result, err := FromReader(strings.NewReader(source))
 	if err != nil {
 		t.Fatal(err.Error())
 	}
